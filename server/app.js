7653dbbf6211babf23064723a25612ed84216e69
md-prog <progmad@outlook.com>
2019-05-14 08:04:04 +0200

    mock server: returning index.html for unknown routes

--- server/app.js
+++ server/app.js
@@ -5,10 +5,6 @@ const portnum = 3000
 
 const proj_root = path.join(__dirname, '../')
 console.log('Project root is set to ' + proj_root)
-
-app.get(['/', '/index'], function (req, res) {
-    res.sendFile(path.join(proj_root, 'client/index.html'))
-})
 app.use('/', express.static(path.join(proj_root, 'client')))
 app.use('/node_modules/pixi.js/bin/pixi.min.js', function (req, res) {
     res.sendFile(path.join(proj_root, 'client/node_modules/pixi.js/bin/pixi.min.js'))
@@ -18,6 +14,11 @@ app.use('/node_modules/three/build/three.min.js', function (req, res) {
 })
 app.use('/node_modules', express.static(path.join(proj_root, 'node_modules')))
 app.use('/json', express.static(path.join(proj_root, 'server/json')))
+
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(proj_root, 'client/index.html'))
+})
 
 app.listen(portnum)
 console.log('server started at ' + portnum);
