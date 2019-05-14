58ea12769590c29a3578ba609ea92da8e2ffe198
md-prog <progmad@outlook.com>
2019-05-14 05:07:36 +0200

    custom build of node modules

--- server/app.js
+++ server/app.js
@@ -6,8 +6,15 @@ const portnum = 3000
 const proj_root = path.join(__dirname, '../')
 console.log('Project root is set to ' + proj_root)
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(proj_root, 'client'))
+app.get(['/', '/index'], function (req, res) {
+    res.sendFile(path.join(proj_root, 'client/index.html'))
+})
+app.use('/', express.static(path.join(proj_root, 'client')))
+app.use('/node_modules/pixi.js/bin/pixi.min.js', function (req, res) {
+    res.sendFile(path.join(proj_root, 'client/node_modules/pixi.js/bin/pixi.min.js'))
+})
+app.use('/node_modules/three/build/three.min.js', function (req, res) {
+    res.sendFile(path.join(proj_root, 'client/node_modules/three/build/three.min.js'))
 })
 app.use('/node_modules', express.static(path.join(proj_root, 'node_modules')))
 app.use('/json', express.static(path.join(proj_root, 'server/json')))
